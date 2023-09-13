57b5e8e12d4c285073343938570675f3d4049515
Ramy Tanios <ramy.tanios@gmail.com>
2023-09-13 22:06:16 +0200

    wip

--- live-server.scala
+++ live-server.scala
@@ -2,28 +2,30 @@
 //> using dep org.http4s::http4s-ember-server:1.0.0-M40
 //> using dep org.http4s::http4s-dsl:1.0.0-M40
 
-import cats.effect.kernel.Async
-import cats.effect.kernel.Concurrent
+import cats.effect.kernel.*
 import fs2.io.file.Files
 import org.http4s.server.websocket.WebSocketBuilder2
 import cats.effect.std.Queue
-import org.http4s.dsl.Http4sDsl
-import org.http4s.HttpRoutes
 import org.http4s.websocket.WebSocketFrame
-import org.http4s.StaticFile
-import cats.effect.kernel.Resource
-import com.comcast.ip4s.Host
-import com.comcast.ip4s.Port
+import com.comcast.ip4s.*
 import org.http4s.ember.server.EmberServerBuilder
 import fs2.io.file.Path
-import cats.syntax.all._
+import cats.syntax.all.*
+import org.typelevel.log4cats.LoggerFactory
+import org.http4s.dsl.io.*
+import org.typelevel.log4cats.slf4j.Slf4jFactory
+import cats.effect.IO
+import org.http4s._
+import org.http4s.dsl._
+import org.http4s.dsl.impl.Responses.NotFoundOps
+import cats.effect.std.Console
+import scala.io.AnsiColor
 
-object LiveServer {
+object LiveServer extends cats.effect.IOApp.Simple {
 
   final class Websocket[F[_]](ws: WebSocketBuilder2[F], sq: Queue[F, String])(
-      implicit F: Async[F]
+      using F: Async[F]
   ) extends Http4sDsl[F] {
-
     val routes: HttpRoutes[F] = HttpRoutes.of[F] { case GET -> Root =>
       val send: fs2.Stream[F, WebSocketFrame] =
         fs2.Stream
@@ -31,73 +33,98 @@ object LiveServer {
           .map(message => WebSocketFrame.Text(message))
       val receive: fs2.Pipe[F, WebSocketFrame, Unit] =
         is => is.evalMap { _ => F.unit }
-
       ws.build(send, receive)
     }
   }
 
-  final class StaticFileServer[F[_]: Files: Concurrent](entryFile: Option[String]) {
-    private val indexHtml: HttpRoutes[F] =
-      for {
-        injected <- Files[F]
-          .readAll(Path("injected.html"))
-          .through(fs2.text.utf8Decode)
-          .compile
-          .lastOrError
+  final class StaticFileServer[F[_]: Files: LoggerFactory](
+      entryFile: Option[String]
+  )(using F: Concurrent[F])
+      extends Http4sDsl[F] {
+    private val static: HttpRoutes[F] = HttpRoutes.of[F] {
+      case GET -> Root =>
+        for {
+          injected <- Files[F]
+            .readAll(fs2.io.file.Path("injected.html"))
+            .through(fs2.text.utf8Decode)
+            .compile
+            .lastOrError
 
-        index0 <- StaticFile
-          .fromPath(Path(entryFile.getOrElse(".")))
-          .getOrElseF(NotFound())
+          indexRsp <- StaticFile
+            .fromPath[F](fs2.io.file.Path(entryFile.getOrElse(".")))
+            .getOrElseF(NotFound())
 
-        index <- F.fromOption(
-          for {
-            htmlBody0 <- index0.split("<html>").lastOption
-            htmlBody <- htmlBody0.split("</html>").lastOption
-          } yield s"<html>$htmlBody$injected</html>",
-          new RuntimeException("Failed to serve index.html")
+          indexBody <- indexRsp.body
+            .through(fs2.text.utf8Decode)
+            .compile
+            .lastOrError
+
+          index <- F.fromOption(
+            for {
+              htmlBody0 <- indexBody.split("<html>").lastOption
+              htmlBody <- htmlBody0.split("</html>").lastOption
+            } yield s"<html>$htmlBody$injected</html>",
+            new RuntimeException("Failed to serve index.html")
+          )
+
+        } yield indexRsp.copy(entity =
+          Entity.stream[F](
+            fs2.Stream.emit(index).through(fs2.text.utf8Encode)
+          )
         )
 
-        route <- index0.flatMap { b => b.body.replace(index) }
-      } yield route
+      case GET -> Root / fileName =>
+        StaticFile
+          .fromPath[F](fs2.io.file.Path(fileName))
+          .getOrElseF(NotFound())
+    }
 
-    val routes = indexHtml
+    val routes = static
   }
 
-  def runF[F[_]: Async: Files]: F[Unit] =
+  def runF[F[_]: Files](using F: Async[F], C: Console[F]): F[Unit] = {
+    implicit val loggerFactory: LoggerFactory[F] = Slf4jFactory.create[F]
     for {
-      host <- Resource.eval {
-        Async[F].fromOption(
+      host <-
+        F.fromOption(
           Host.fromString("localhost"),
           new IllegalArgumentException("Invalid host")
         )
-      }
 
-      port <- Resource.eval {
-        Async[F].fromOption(
+      port <-
+        F.fromOption(
           Port.fromInt(8090),
           new IllegalArgumentException("Invalid port")
         )
-      }
 
       wsOut <- cats.effect.std.Queue.unbounded[F, String]
 
       _ <- Files[F]
         .watch(fs2.io.file.Path("."))
-        .evalMap { _ => wsOut.offer("reload") }
+        .evalMap { _ =>
+          wsOut.offer("reload").flatTap { _ =>
+            C.println(s"${AnsiColor.CYAN}Changes detected${AnsiColor.RESET}")
+          }
+        }
         .compile
         .drain
 
-      allRoutes = (wsb: WebSocketBuilder2) => {
+      allRoutes = (wsb: WebSocketBuilder2[F]) => {
         val ws = new Websocket[F](wsb, wsOut).routes
-        val static = new StaticFileServer[F]().routes
-        ws <+> static
+        val static = new StaticFileServer[F](none[String]).routes
+        (ws <+> static).orNotFound
       }
 
-      server <- EmberServerBuilder
+      _ <- EmberServerBuilder
         .default[F]
         .withHost(host)
         .withPort(port)
         .withHttpWebSocketApp(wsb => allRoutes(wsb))
+        .build
+        .use { _ => Async[F].never }
 
-    } yield server
+    } yield ()
+  }
+
+  override def run: IO[Unit] = runF[IO]
 }
